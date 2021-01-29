e53d973442fb5a1fb8104790bd926d352d8470c2
Anatoly Siniakov <siniakov457@gmail.com>
2021-01-29 21:14:32 +0300

    raw email impl added @Value

---
+++ server/src/main/java/com/edu_netcracker/cmp/configs/MailConfig.java
@@ -0,0 +1,69 @@
+package com.edu_netcracker.cmp.configs;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+@Getter
+@Setter
+@PropertySource("classpath:mail.properties")
+public class MailConfig {
+
+    @Value("${mail.host}")
+    private String host;
+
+    @Value("${mail.port}")
+    private int port;
+
+    @Value("${mail.username}")
+    private String username;
+
+    @Value("${mail.password}")
+    private String password;
+
+    @Value("${mail.protocol}")
+    private String protocol;
+
+    @Value("${mail.smtp.debug}")
+    private boolean debug;
+
+    @Value("${mail.smtp.starttls.enable}")
+    private boolean starttls;
+
+    @Value("${mail.smtp.starttls.required}")
+    private boolean starttls_required;
+
+    @Value("${mail.from}")
+    private String from;
+
+    @Value("${mail.smtp.auth}")
+    private String auth;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        Properties mailProperties = new Properties();
+        mailProperties.put("mail.smtp.auth", auth);
+        mailProperties.put("mail.smtp.starttls.enable", starttls);
+        mailProperties.put("mail.smtp.starttls.required", starttls_required);
+        mailProperties.put("mail.smtp.debug", debug);
+
+        mailSender.setJavaMailProperties(mailProperties);
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setProtocol(protocol);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+        return mailSender;
+    }
+
+}
